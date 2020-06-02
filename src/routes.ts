777b528b5dfb112a63b2ba5c811de359143e7c0c
Anderson Rodrigues <anddersonrds@gmail.com>
2020-06-02 18:42:19 -0300

    add editorconfig, eslint and prettier

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const routes = express.Router();
 
 routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello World'});
+  return response.json({ message: 'Hello World' });
 });
 
 export default routes;
