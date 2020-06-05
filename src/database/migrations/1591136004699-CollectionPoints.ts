2b4aa7dfe04aae8f63d1b123466e1f3ad0b1a660
Anderson Rodrigues <anddersonrds@gmail.com>
2020-06-04 23:58:02 -0300

    add migrations

---
+++ src/database/migrations/1591136004699-CollectionPoints.ts
@@ -0,0 +1,69 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CollectionPoints1591136004699
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";');
+    await queryRunner.createTable(
+      new Table({
+        name: 'collection_points',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'image',
+            type: 'varchar',
+          },
+          {
+            name: 'whatsapp',
+            type: 'varchar',
+          },
+          {
+            name: 'latitude',
+            type: 'decimal',
+          },
+          {
+            name: 'longitude',
+            type: 'decimal',
+          },
+          {
+            name: 'city',
+            type: 'varchar',
+          },
+          {
+            name: 'uf',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('collection_points');
+  }
+}
---
+++ src/database/migrations/1591325624911-PointItems.ts
@@ -0,0 +1,42 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class PointItems1591325624911 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'point_items',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'collection_point_id',
+            type: 'integer',
+          },
+          {
+            name: 'item_id',
+            type: 'integer',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('point_items');
+  }
+}
